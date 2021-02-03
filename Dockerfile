4d6e6c1ed6afd5482dbc748148a1aceaf2697303
skyclo <hello.skyclo@gmail.com>
2021-02-03 08:47:34 -0500

    🚧 wip(marina-core): add container spawns

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM ubuntu:latest
 RUN useradd -m -u 8877 -s /bin/bash ideoxan-user
 USER ideoxan-user
+COPY ./ideoxan.txt /home/ideoxan-user/ideoxan.txt
--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const http = require('http')
 
+const util = require('util')
+
+const exec = util.promisify(require('child_process').exec)
+
 const pty = require('node-pty')
 
 
@@ -14,14 +18,31 @@ const io = require('socket.io')(server)
 
 
 
+app.use('/modules/', express.static('./node_modules'))
+
+
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/terminal.html')
 })
 
-io.on('connection', (socket) => {
-    const CONTAINER_NAME = 'sharp_cray'
-    const terminal = pty.spawn('docker', `exec -it ${CONTAINER_NAME} /bin/bash`, {})
-    console.log('Connecting to Remote Sandbox Instance...')
+io.on('connection', async (socket) => {
+    socket.emit('stdout', 'Building Sandbox Image...')
+    socket.emit('stdout', '\t(1/6) Loading Sandbox Configuration...')
+    socket.emit('stdout', '\t(2/6) Pulling Image...')
+    socket.emit('stdout', '\t(3/6) Creating User...')
+    socket.emit('stdout', '\t(4/6) Adding Dependencies and Packages...')
+    socket.emit('stdout', '\t(5/6) Cleaning Up...')
+    socket.emit('stdout', '\t(6/6) Exporting...')
+
+    socket.emit('stdout', 'Spawning Sandbox Instance...')
+    let containerRun = await exec('docker run -d -t marina-docker')
+    if (containerRun.stderr) return socket.emit('stderr', 'Spawn Failed. Exiting.')
+
+    socket.emit('stdout', 'Connecting to Sandbox Instance...')
+    const CONTAINER_HASH = containerRun.stdout.toString().substring(0, 12)
+    const terminal = pty.spawn('docker', `exec -it ${CONTAINER_HASH} /bin/bash`, {})
+    socket.emit('stdout', 'Connected.')
 
     terminal.onData((data) => {
         socket.emit('stdout', data)
@@ -31,8 +52,8 @@ io.on('connection', (socket) => {
         terminal.write(data)
     })
 
-    socket.on('disconnect', (reason) => {
+    socket.on('disconnect', async (reason) => {
+        await exec(`docker stop ${CONTAINER_HASH}`)
         terminal.kill()
-        console.log('Dismounting Remote Sandbox Instance...')
     })
 })
