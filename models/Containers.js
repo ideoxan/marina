5548e856958c51e120415fc5a6425361bf1425f3
skyclo <hello.skyclo@gmail.com>
2021-02-05 08:20:13 -0500

    🗃 feat(core): add container schema

---
+++ models/Containers.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const ContainerSchema = new mongoose.Schema({ 
+    uid: {
+        type: String,
+        required: true
+    },
+    containerID: {
+        type: String,
+        required: true
+    },
+    lessonPath: {
+        type: String,
+        required: true
+    },
+    expires: {
+        type: Number,
+        required: true,
+        default: -1
+    }
+})
+
+module.exports = mongoose.model('containers', ContainerSchema)
