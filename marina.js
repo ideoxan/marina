7f81ac072c5f59d8af6c0475a4ede3364e8dcd5e
Skyclo <hello.skyclo@gmail.com>
2021-02-09 22:17:45 -0500

    💡 chore(core): add comments

--- marina.js
+++ marina.js
@@ -41,22 +41,43 @@ const { v4: uuidv4 } = require('uuid')
 
 
 
-const socketOpts = {
+/* ---------------------------------------------------------------------------------------------- */
+/*                                  CONSTANTS AND INITIALIZATIONS                                 */
+/* ---------------------------------------------------------------------------------------------- */
+/* -------------------------------------- WebSocket Server -------------------------------------- */
+// This sets up the Websocket server (using the socket.io package).
+const PORT = 42550                                              // Port the ws server will run on
+const socketOpts = {                                            // Server options
     cors: {
         origin: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true
     }
 }
-const io = socketIO(socketOpts).listen(42550)
+const io = socketIO(socketOpts).listen(PORT)                   // Opens the server on the PORT
 
+/* ------------------------------------- MongoDB (Database) ------------------------------------- */
+// Connects to the local or internet database (I suggest local btw) using valid mongo uri. Uses
+// Mongoose drivers for Mongo DB
 mongoose.connect('mongodb://localhost:27017/ix', {
     useNewUrlParser: true,                      // Required
     useUnifiedTopology: true                    // Required
 })
+
+/* --------------------------------------- Task Scheduler --------------------------------------- */
+// This task scheduler is powered by the mongo-scheduler-more package. It polls the database every
+// 60 seconds (1 minute) to check if a task is to be completed. The interval for polling is quite
+// large to reduce load on the database and encourage subsequent overlapping calls to the DB.
 const scheduler = new MSM('mongodb://localhost:27017/ix', {
     pollInterval: 60000
 })
+
+/* -------------------------------------- Base Image Setup -------------------------------------- */
+// This runs the docker build command to build the base image of marina. The dockerfile for the
+// base image can be found under the root directory. It is tagged "marina-docker" so it can be
+// easily targeted. It also overrides any existing base image tagged with "marina-docker". The
+// marina base image is based off of the ubuntu-latest image. In reality, any other image could be
+// used but Ubuntu was chosen due to its ease of use and appeal to beginners. 
 exec('docker build --tag marina-docker .')
 
 
